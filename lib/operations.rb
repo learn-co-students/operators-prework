435cada233c6bb387bacb4758e12d430b835a59d
Isaac Anderson <isaac.s.anderson@gmail.com>
2017-03-16 19:23:42 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
+#require 'pry'
+
 def unsafe?(speed)
-
+	return true if speed < 40 || speed > 60
+	#binding.pry
+	false
 end
-
-
 
 def not_safe?(speed)
-	
+	#binding.pry
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
