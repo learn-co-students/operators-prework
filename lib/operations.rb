a0a0fe306341ee24aeeffc33a45faa12cbb074e8
dhakle <vacarlos_alm@hotmail.com>
2017-06-10 00:17:46 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+	speed > 60 ? true : false
+	speed < 40 ? true : false
+	speed < 60 && speed > 40 ? false : true
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? true : false
+	speed > 40 && speed < 60 ? false : true 
 end
 	
 
