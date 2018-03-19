6019fd83968fcc3f4401290c554c1851e4962065
jessebillard <jessebillard@mac.com>
2018-03-19 14:56:55 -0400

    Operators

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,21 @@
 def unsafe?(speed)
-
+  if speed > 60
+    answer = true
+    answer
+  elsif speed < 40
+    answer = true
+    answer
+  elsif speed < 60 && speed > 40
+    answer = false
+    answer
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+    speed > 60 || speed < 40 ? answer = true : answer = false
+    answer
 end
 	
 
