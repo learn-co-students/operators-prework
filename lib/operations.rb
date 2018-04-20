36dd9a5edb371d4366716a61e2f4ffc641eefc75
github username <github email address>
2018-04-19 20:50:53 -0700

    Created #unsafe? and #not_safe?. Included conditionals for both.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+	if (speed > 60) || (speed < 40)
+		"Drive Savely Please!"
+	else
+		"Thank you for being reasonable."
 end
 
 
 
 def not_safe?(speed)
-	
+	((speed > 60) || (speed < 40))? "Drive Savely Please!" : "Thank you for being reasonable."
 end
-	
-
-
