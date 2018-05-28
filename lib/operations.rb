85430c68ce9dfb61254145062dd58e24207acf20
Laura Adam <laurawynosky@gmail.com>
2018-05-28 22:35:10 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed <40
+    return true
+  elsif speed>60
+    return true
+  else
+    return false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+ return (speed <40) and (speed >60) ? true : false
 end
-	
-
-
