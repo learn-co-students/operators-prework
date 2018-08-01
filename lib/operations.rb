8b27e9b767aabaf3db6e925ab7551cb7c701898b
thexyman <makdaa1@gmail.com>
2018-08-01 17:38:05 +0100

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
-def unsafe?(speed)
 
+def unsafe?(n)
+	if n < 40 || n > 60
+		return true
+	else
+		return false
+	end
 end
 
-
-
-def not_safe?(speed)
-	
+def not_safe?(n)
+	n < 40 || n > 60 ? true : false
 end
-	
-
-
