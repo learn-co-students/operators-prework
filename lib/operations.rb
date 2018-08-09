1622300b854d4ca04a980bcbb6709791abe4e19a
Nicky Liu <nickyliu91@gmail.com>
2018-08-09 10:56:07 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    return true
+  end
+  false
 end
 
 
 
 def not_safe?(speed)
-	
+  dangerous = "true" if speed < 40 || speed > 60
+  dangerous == "true" ? true : false
 end
-	
-
-
