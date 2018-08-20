553bb3078d82b899213de6f2a269f9986f001bc8
Frida Casas <fridakahsas@yahoo.com>
2018-08-20 21:43:25 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+  if 
+    speed > 60
+    return true
+  else if 
+    speed < 40 
+    return true
+  else  speed == 40.. 60
+    return false
+  end
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed > 60 ? true :  false 
+ speed < 40 ? true : false
+ else
+   speed > 60 || speed < 40 ? true : false
 end
-	
-
-
+ 
