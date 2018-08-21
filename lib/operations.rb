16d48b382ae617056d9506f9a29e2afcba206c30
sparkbold-git <sparkboldstudio@gmail.com>
2018-08-21 04:19:40 -0500

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-
+	if (speed > 60 || speed < 40)
+		answer = !!"unsafe"
+	else
+		answer = !"safe"
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40? !!"not safe":!"safe"
 end
-	
-
-
