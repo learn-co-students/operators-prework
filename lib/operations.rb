048c55a7f20b42483a4f6cc74579386e3034c463
thompson-ad <thompson_ad@outlook.com>
2018-09-04 22:33:59 +0100

    operators final

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
-def unsafe?(speed)
+#The first method, unsafe? will take in an argument of a speed and return true #if the speed is unsafe and false if it is safe.
 
+def unsafe?(speed)
+	if speed < 40 || speed > 60
+		true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+
+	speed < 40 || speed > 60 ? true : false
+
 end
-	
-
-
