360b741f52a44ee7550bc70ebd756ccd15ac709c
Matthew Dizon <matthewdizon94@gmail.com>
2018-09-06 19:28:17 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
+=begin
+Use an if/else statement pair to build the unsafe? method.
+ It should return true if the speed is either below 40 or above 60.
+ would return false as that's within the "safe" range
+=end
 def unsafe?(speed)
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
+	end
 
 end
 
 
 
 def not_safe?(speed)
-	
+
+	speed  < 40 || speed > 60  ? true : false
+
 end
-	
-
-
