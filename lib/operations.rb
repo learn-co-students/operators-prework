c5fd9e9b558288dd0fe7e8276f8f68f7f7423125
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-01 16:58:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,14 +11,12 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60 
-  return true 
-  elsif speed < 40
-  return true
-  else speed.between?(40,60) 
-  return false
-  end
+ speed > 60 || speed<40 ? true: false
 end
+
 	
+	
+
+
 
 
