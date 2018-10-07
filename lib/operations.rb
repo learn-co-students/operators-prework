510492f486b7d546afa06ba519f4624c2327f04f
github username <github email address>
2018-10-06 23:19:57 -0400

    My first commit

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
+require 'pry'
+
 def unsafe?(speed)
-
+  if speed < 40 
+    # use 'binding.pry' to check/debug code in the terminal
+    true
+  elsif speed > 60
+    true
+  else 
+    false
+  end 
 end
-
 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60  ? true : false 
 end
-	
-
-
