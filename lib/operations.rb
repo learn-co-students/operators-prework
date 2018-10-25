1f6c6293f5899ad0f7fa7b4bc4fdd0007bba0f7e
github username <crishanks12@gmail.com>
2018-10-24 22:16:01 -0600

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
+	end
 end
+unsafe?(35)
 
 
 
 def not_safe?(speed)
-	
+	return speed < 40 || speek > 60 ? true : false
 end
-	
-
-
+not_safe?(35)
