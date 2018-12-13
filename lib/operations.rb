3c3b7f01827b72c569e405ef216b4b05c7d09af5
cyn7117 <cyn7117@gmail.com>
2018-12-13 20:46:32 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,12 +11,8 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60
-    true
-  elsif speed < 40
-    true
-  else speed > 40 && speed < 60 ? false: true
-  end
+  speed > 40 && speed < 60 ? false: true
+ 
 	
 end
 	
