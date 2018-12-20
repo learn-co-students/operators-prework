cbe3a3b6de02aaf5d763efbdc64e8ec6248c3e94
mkay0961 <mkay0961@gmail.com>
2018-12-20 16:57:18 -0500

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,26 @@
 def unsafe?(speed)
 
-end
 
+		if(speed > 60)
+			return true
+		end
+
+		if(speed < 40)
+			return true
+		end
+
+		if (speed > 40) && (speed < 60)
+			return false
+		end
+
+
+
+
+end
 
 
 def not_safe?(speed)
-	
+
+	(speed > 40) && (speed < 60) ? false : true
+
 end
-	
-
-
