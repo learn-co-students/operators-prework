c6cd37fa377b4b06067b895d5b901b0300e589e4
Your Name <myung.kim287@gmail.com>
2019-01-07 20:17:48 -0800

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+	# !(speed > 40 && speed < 60)
+	if speed < 40 || speed > 60
+		true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 40 && speed < 60 ? false : true
 end
-	
-
-
