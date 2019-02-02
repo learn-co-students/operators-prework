36cdf9223210f44119db613b943bc983f3feeba3
Errol W <ewatsonrei@gmail.com>
2019-02-02 23:17:40 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,23 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  elsif speed < 60 && speed > 40
+    return false
+  else
+    return "Unexpected condition!"
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+	if speed > 60
+	  return true
+	 elsif speed < 40
+	   return true
+	 elsif speed > 40 && speed < 60
+	   return false
+  end
 end
 	
 
