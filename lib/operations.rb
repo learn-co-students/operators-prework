53c2031494a11189806ff3ae33a22a265a67bf6c
Errol W <ewatsonrei@gmail.com>
2019-02-02 23:31:34 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,5 @@
+#Something about the require keyword may be missing in this file 
+
 def unsafe?(speed)
   if speed > 60
     return true
@@ -11,26 +13,9 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	if speed > 60 || speed < 40 ? return true : false
+	if speed > 60 || speed < 40 ? true : false
 	  return true
-	# elsif speed > 40 && speed < 60
-	  # return false
-  end
-end
-
-=begin
-def not_safe?(speed)
-	if speed > 60
-	  return true
-	 elsif speed < 40
-	   return true
 	 elsif speed > 40 && speed < 60
 	   return false
   end
 end
-=end
-
-	
-
-
-
