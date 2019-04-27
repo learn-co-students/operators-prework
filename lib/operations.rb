cfbe0b8ecaafc312cc1379b91e28a56ce6f529ae
Derek Townsend <derekjavon2@gmail.com>
2019-04-27 17:19:58 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+  unsafe=false
+  if (speed<40||speed>60)
+    unsafe=true
+  end
+  return unsafe
 end
 
 
 
 def not_safe?(speed)
-	
+  (speed<40||speed>60) ? unsafe=true:unsafe=false
+
+  return unsafe
 end
-	
-
-
