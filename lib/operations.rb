9fafd1127ea87558f8243045c1e874f3d2488677
Dylan Harris <dylanharris23@gmail.com>
2019-05-27 05:49:40 +0000

    WIP

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+if speed > 60
+  return !!speed
+elsif speed < 60
+  return !!speed
+else speed == 50
+  return !!speed
 end
-
+end
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
